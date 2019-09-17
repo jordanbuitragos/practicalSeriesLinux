98719fbe847a301951d8c832416832a92ea4a22a
jordan buitrago <jordanbuitragos@gmail.com>
2019-09-16 23:15:08 -0500

    impresionDeArchivosCLI

---
+++ imprimirArchivosCLI.sh
@@ -0,0 +1,7 @@
+#   Los documentos se imprimen con el comando lpr. Para imprimir un documento de texto, utilice el comando lpr junto con la opcion -P (impresora) para seleccionar una impresora asi:
+
+lpr -Pmylp mydocument
+
+cal 2019 | lpr
+
+
